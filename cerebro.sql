d1ac768ee082b3b046b2ce1fdab4d31b5863ebd1
lweisse <llweisse@gmail.com>
2013-04-01 22:04:25 -0400

    Add authorship table, rename roles to permissions, create roles table to go with authorship, remove writer etc

--- cerebro.sql
+++ cerebro.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 01, 2013 at 10:21 PM
+-- Generation Time: Apr 02, 2013 at 02:07 AM
 -- Server version: 5.5.8
 -- PHP Version: 5.3.5
 
@@ -22,26 +22,22 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `artist`
+-- Table structure for table `authorship`
 --
 
-CREATE TABLE IF NOT EXISTS `artist` (
+CREATE TABLE IF NOT EXISTS `authorship` (
   `firstname` varchar(128) DEFAULT NULL,
   `lastname` varchar(128) DEFAULT NULL,
-  `artistID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`artistID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `authorshipID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `comicID` int(11) DEFAULT NULL,
+  `roleID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`authorshipID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `artist`
+-- Dumping data for table `authorship`
 --
 
-INSERT INTO `artist` (`firstname`, `lastname`, `artistID`) VALUES
-('Fiona', 'Staples', 1),
-('Ramon', 'Perez', 2),
-('Robb', 'Mommaerts', 3),
-('Stuart', 'Immonen', 4),
-('Chris', 'Samnee', 5);
 
 -- --------------------------------------------------------
 
@@ -54,7 +50,7 @@ CREATE TABLE IF NOT EXISTS `collector` (
   `lastname` varchar(128) DEFAULT NULL,
   `username` varchar(128) DEFAULT NULL,
   `email` varchar(128) DEFAULT NULL,
-  `roleID` smallint(6) DEFAULT NULL,
+  `permissionID` smallint(6) DEFAULT NULL,
   `collectorID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `password` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`collectorID`),
@@ -65,32 +61,9 @@ CREATE TABLE IF NOT EXISTS `collector` (
 -- Dumping data for table `collector`
 --
 
-INSERT INTO `collector` (`firstname`, `lastname`, `username`, `email`, `roleID`, `collectorID`, `password`) VALUES
+INSERT INTO `collector` (`firstname`, `lastname`, `username`, `email`, `permissionID`, `collectorID`, `password`) VALUES
 ('Laurinda', 'Weisse', 'lweisse', 'testing', NULL, 5, 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'),
 ('test', 'test', 'test', 'test', NULL, 21, 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `colorist`
---
-
-CREATE TABLE IF NOT EXISTS `colorist` (
-  `firstname` varchar(128) DEFAULT NULL,
-  `lastname` varchar(128) DEFAULT NULL,
-  `coloristID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`coloristID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
-
---
--- Dumping data for table `colorist`
---
-
-INSERT INTO `colorist` (`firstname`, `lastname`, `coloristID`) VALUES
-('Laura', 'Martin', 2),
-('Rainer', 'Petter', 3),
-('Marte', 'Gracia', 4),
-('Javier', 'Rodriguez', 5);
 
 -- --------------------------------------------------------
 
@@ -101,13 +74,6 @@ INSERT INTO `colorist` (`firstname`, `lastname`, `coloristID`) VALUES
 CREATE TABLE IF NOT EXISTS `comic` (
   `seriesID` int(10) unsigned DEFAULT NULL,
   `subtitle` varchar(128) DEFAULT NULL,
-  `writerID` int(10) unsigned DEFAULT NULL,
-  `artistID` int(11) DEFAULT NULL,
-  `inkerID` int(10) DEFAULT NULL,
-  `coloristID` int(10) DEFAULT NULL,
-  `lettererID` int(10) DEFAULT NULL,
-  `coverartistID` int(6) DEFAULT NULL,
-  `covercoloristID` int(10) DEFAULT NULL,
   `volume` smallint(6) DEFAULT NULL,
   `number` smallint(6) DEFAULT NULL,
   `limitedseries` varchar(128) DEFAULT NULL,
@@ -118,7 +84,6 @@ CREATE TABLE IF NOT EXISTS `comic` (
   `comicID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `isbn` bigint(11) DEFAULT NULL,
   `adddate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `coverinker` int(11) DEFAULT NULL,
   PRIMARY KEY (`comicID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
@@ -126,78 +91,14 @@ CREATE TABLE IF NOT EXISTS `comic` (
 -- Dumping data for table `comic`
 --
 
-INSERT INTO `comic` (`seriesID`, `subtitle`, `writerID`, `artistID`, `inkerID`, `coloristID`, `lettererID`, `coverartistID`, `covercoloristID`, `volume`, `number`, `limitedseries`, `pubmonth`, `pubyear`, `familyID`, `publisherID`, `comicID`, `isbn`, `adddate`, `coverinker`) VALUES
-(4, NULL, 2, 1, NULL, NULL, 1, 1, NULL, 1, 11, NULL, 0, 2013, NULL, 3, 1, 985301166, '2013-03-27 12:01:01', NULL),
-(3, NULL, 4, 2, NULL, 2, 2, 2, 2, 1, 26, NULL, 0, 2013, 1, 1, 2, 2147483647, '2013-03-27 12:05:08', NULL),
-(5, NULL, 5, 3, 1, 3, 3, 3, NULL, 1, 26, NULL, 0, 2012, NULL, 2, 3, 1617682148, '2013-03-27 12:09:32', NULL),
-(2, NULL, 2, 4, 2, 4, 4, 4, 1, 1, 9, NULL, 0, 2013, 1, 1, 4, 5960607900, '2013-03-27 12:17:19', NULL),
-(2, NULL, 2, 4, 2, 4, 4, 4, 1, 1, 5, NULL, 0, 2013, 1, 1, 5, NULL, '2013-03-27 12:19:03', NULL),
-(2, NULL, 2, 4, 2, 4, 4, 4, 1, 1, 6, NULL, 0, 2013, 1, 1, 7, NULL, '2013-03-27 12:20:07', NULL),
-(1, NULL, 3, 5, NULL, 5, 2, 6, 3, 3, 24, NULL, 0, 2013, NULL, 1, 8, NULL, '2013-03-27 12:21:57', NULL);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `coverartist`
---
-
-CREATE TABLE IF NOT EXISTS `coverartist` (
-  `firstname` varchar(128) DEFAULT NULL,
-  `lastname` varchar(128) DEFAULT NULL,
-  `coverartistID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`coverartistID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
-
---
--- Dumping data for table `coverartist`
---
-
-INSERT INTO `coverartist` (`firstname`, `lastname`, `coverartistID`) VALUES
-('Fiona', 'Staples', 1),
-('Ramon', 'Perez', 2),
-('Robb', 'Mommaerts', 3),
-('Stuart', 'Immonen', 4),
-('Chris', 'Samnee', 6);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `covercolorist`
---
-
-CREATE TABLE IF NOT EXISTS `covercolorist` (
-  `firstname` varchar(128) DEFAULT NULL,
-  `lastname` varchar(128) DEFAULT NULL,
-  `coverartistID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`coverartistID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
-
---
--- Dumping data for table `covercolorist`
---
-
-INSERT INTO `covercolorist` (`firstname`, `lastname`, `coverartistID`) VALUES
-('Marte', 'Gracia', 1),
-('Laura', 'Martin', 2),
-('Javier', 'Rodriguez', 3);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `coverinker`
---
-
-CREATE TABLE IF NOT EXISTS `coverinker` (
-  `firstname` varchar(128) DEFAULT NULL,
-  `lastname` varchar(128) DEFAULT NULL,
-  `coverinkerID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`coverinkerID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `coverinker`
---
-
+INSERT INTO `comic` (`seriesID`, `subtitle`, `volume`, `number`, `limitedseries`, `pubmonth`, `pubyear`, `familyID`, `publisherID`, `comicID`, `isbn`, `adddate`) VALUES
+(4, NULL, 1, 11, NULL, 0, 2013, NULL, 3, 1, 985301166, '2013-03-27 12:01:01'),
+(3, NULL, 1, 26, NULL, 0, 2013, 1, 1, 2, 2147483647, '2013-03-27 12:05:08'),
+(5, NULL, 1, 26, NULL, 0, 2012, NULL, 2, 3, 1617682148, '2013-03-27 12:09:32'),
+(2, NULL, 1, 9, NULL, 0, 2013, 1, 1, 4, 5960607900, '2013-03-27 12:17:19'),
+(2, NULL, 1, 5, NULL, 0, 2013, 1, 1, 5, NULL, '2013-03-27 12:19:03'),
+(2, NULL, 1, 6, NULL, 0, 2013, 1, 1, 7, NULL, '2013-03-27 12:20:07'),
+(1, NULL, 3, 24, NULL, 0, 2013, NULL, 1, 8, NULL, '2013-03-27 12:21:57');
 
 -- --------------------------------------------------------
 
@@ -240,27 +141,6 @@ CREATE TABLE IF NOT EXISTS `image` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `inker`
---
-
-CREATE TABLE IF NOT EXISTS `inker` (
-  `firstname` varchar(128) DEFAULT NULL,
-  `lastname` varchar(128) DEFAULT NULL,
-  `inkerID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`inkerID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `inker`
---
-
-INSERT INTO `inker` (`firstname`, `lastname`, `inkerID`) VALUES
-('Mike', 'Norton', 1),
-('Wade', 'von Grawbadger', 2);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `in_list`
 --
 
@@ -276,29 +156,6 @@ CREATE TABLE IF NOT EXISTS `in_list` (
 -- Dumping data for table `in_list`
 --
 
-
--- --------------------------------------------------------
-
---
--- Table structure for table `letterer`
---
-
-CREATE TABLE IF NOT EXISTS `letterer` (
-  `firstname` varchar(128) DEFAULT NULL,
-  `lastname` varchar(128) DEFAULT NULL,
-  `lettererID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`lettererID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
-
---
--- Dumping data for table `letterer`
---
-
-INSERT INTO `letterer` (`firstname`, `lastname`, `lettererID`) VALUES
-('Fonografiks', NULL, 1),
-('VC''s Joe', 'Caramanga', 2),
-('Tom', 'Long', 3),
-('VC''s Cory', 'Petit', 4);
 
 -- --------------------------------------------------------
 
@@ -372,6 +229,24 @@ CREATE TABLE IF NOT EXISTS `owned` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `permissiontype` varchar(128) DEFAULT NULL,
+  `permissions` varchar(128) DEFAULT NULL,
+  `permissionID` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`permissionID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `permissions`
+--
+
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `publisher`
 --
 
@@ -421,16 +296,25 @@ CREATE TABLE IF NOT EXISTS `review` (
 --
 
 CREATE TABLE IF NOT EXISTS `role` (
-  `roletype` varchar(128) DEFAULT NULL,
-  `permissions` varchar(128) DEFAULT NULL,
-  `roleID` smallint(6) unsigned NOT NULL AUTO_INCREMENT,
+  `roleID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `rolename` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`roleID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `role`
 --
 
+INSERT INTO `role` (`roleID`, `rolename`) VALUES
+(1, 'artist'),
+(2, 'colorist'),
+(3, 'coverartist'),
+(4, 'covercolorist'),
+(5, 'coverinker'),
+(6, 'inker'),
+(7, 'letterer'),
+(8, 'writer'),
+(9, 'editor');
 
 -- --------------------------------------------------------
 
@@ -512,28 +396,3 @@ CREATE TABLE IF NOT EXISTS `tagtype` (
 INSERT INTO `tagtype` (`tagtypeID`, `tagtype`) VALUES
 (1, 'comic'),
 (2, 'list');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `writer`
---
-
-CREATE TABLE IF NOT EXISTS `writer` (
-  `firstname` varchar(128) DEFAULT NULL,
-  `lastname` varchar(128) DEFAULT NULL,
-  `birthyear` smallint(4) DEFAULT NULL,
-  `writerID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`writerID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
-
---
--- Dumping data for table `writer`
---
-
-INSERT INTO `writer` (`firstname`, `lastname`, `birthyear`, `writerID`) VALUES
-('Brian Michael', 'Bendis', NULL, 1),
-('Brian K.', 'Vaughan', NULL, 2),
-('Mark', 'Waid', NULL, 3),
-('Jason', 'Aaron', NULL, 4),
-('Ben', 'McCool', NULL, 5);
