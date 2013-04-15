1d6f7d4bc151c99d94f69c5c1487c63e37640b66
lweisse <llweisse@gmail.com>
2013-04-15 10:41:28 -0400

    Third party log-in mostly working

--- frontend/registration.php
+++ frontend/registration.php
@@ -44,8 +44,39 @@ if ( isset($_POST['firstname']) && isset($_POST['lastname']) &&
 }
 }
 ?>
+<head>
+<script type="text/javascript">
+(function() {
+    if (typeof window.janrain !== 'object') window.janrain = {};
+    if (typeof window.janrain.settings !== 'object') window.janrain.settings = {};
+    
+    janrain.settings.tokenUrl = 'http://localhost/SI664/frontend/janrain2.php';
 
+    function isReady() { janrain.ready = true; };
+    if (document.addEventListener) {
+      document.addEventListener("DOMContentLoaded", isReady, false);
+    } else {
+      window.attachEvent('onload', isReady);
+    }
+
+    var e = document.createElement('script');
+    e.type = 'text/javascript';
+    e.id = 'janrainAuthWidget';
+
+    if (document.location.protocol === 'https:') {
+      e.src = 'https://rpxnow.com/js/lib/cerebro-comics/engage.js';
+    } else {
+      e.src = 'http://widget-cdn.rpxnow.com/js/lib/cerebro-comics/engage.js';
+    }
+
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(e, s);
+})();
+</script>
+</head>
+<body>
 <p>Register</p>
+
 <form method="post">
 <p>First name:
 <input type="text" name="firstname" required></p>
@@ -60,3 +91,4 @@ if ( isset($_POST['firstname']) && isset($_POST['lastname']) &&
 <p><input type="submit" value="Add New"/>
 <a href="index.php">Cancel</a></p>
 </form> 
+<div id="janrainEngageEmbed"></div>
