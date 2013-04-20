128ee9f05c57934dcd198e347c371f47974c119d
sginsbe <sginsbe@umich.edu>
2013-04-20 14:57:36 -0400

    getting rid of stuff i hope

--- frontend/login.php
+++
@@ -1,6 +0,0 @@
-<?php // login.php 
-$db_hostname = 'localhost'; 
-$db_database = 'cerebro'; 
-$db_username = 'sginsbe'; 
-$db_password = 'profx'; 
-?>
--- frontend/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-?>
-<p>Logged out.</p>
-<p><a href="signin.php">Log In</a></p>
-<p><a href="index.php">Take me home</a></p>
