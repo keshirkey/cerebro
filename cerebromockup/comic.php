96a468dfa5507ad8fecbf89df7cf977cffdec775
keshirkey <keshirkey@gmail.com>
2013-04-21 21:53:28 -0400

    rearranged individual comic page code

--- cerebromockup/comic.php
+++ cerebromockup/comic.php
@@ -1,3 +1,49 @@
+<!DOCTYPE html>
+ <head>
+ 	<html lang="en">
+    <meta charset="UTF-8">
+    <title>Cerebro - Your Brain on Comics</title>
+    <base href="http://localhost:8888/cerebro/cerebromockup/">
+    <link rel="stylesheet" type="text/css" href="static/css/styles.css" title="Default Stylesheet" media="all" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="js/jquery.formalize.js"></script>
+ </head>
+ <body>
+ 	<div id="wholecontainer">
+    	<header>
+    		<div id="header">
+    		<!-- search bar -->
+    		<div id="searchwrap">
+        		<input id="searchbox" type="search" />
+                <img src="static/images/searchicon.png" alt="Search Icon" height="19" width="19">
+        		<a href="advancedsearch.html">Advanced Search</a>
+        	</div>
+
+        	<div id="logo">
+        		<a href="index.php"><img src="static/images/cerebro_logo.gif" alt="Cerebro Logo"></a>
+        	</div>
+
+        	<!-- nav links -->
+        	<nav>
+            	<ul>
+                	<li><a href="index.php">Home</a></li>
+                	<li><a href="about.html">About</a></li>
+					<?php if (!isset($_SESSION['username'])){ ?>
+                	<li><a href="signin.php">Log In</a></li>
+					<li><a href="registration.php"></a></li>
+					<?php } ?>
+					<?php if ( isset($_SESSION['username'])){ ?>
+					<li><a href="logout.php">Logout</a></li>
+					<!-- Welcome user not quite working yet -->
+					<li>Welcome <?php '.htmlentities($_SESSION["username"]). ' ?></li>
+					<?php } ?>
+					   
+            	</ul>
+        	</nav>
+        	</div>
+    	</header>
+
+        <section id="maincontent">
 <?php
 require_once "login.php";
 session_start();
@@ -54,21 +100,21 @@ echo('<div class="cover">'."\n");
 if ($image_row[0] == NULL) {
     echo('<a href="comic.php?comicID='.$comicID.'"><img src="static/images/filler_woman.gif"></a>'."\n");
     }
-	
+    
 else {
     echo('<a href="comic.php?comicID='.$comicID.'"><img src="'.$image_row[0].'"></a>'."\n");
     }
-echo("</div>\n");
+echo("</div></div>\n");
 echo('<div id="comicinfo">'."\n");
 echo('<div class="rowone"><h4><span class="alignleft">'."\n");
 echo($series_row[0]);
 echo('<div class="rowone"><h4><span class="alignleft">'."\n");
 //display subtitle
-Magellan#1
+
 if($row[8] != NULL){
-	echo($row[8]);
-	echo('<div class="rowone"><h4><span class="alignleft">'."\n");
-	}
+    echo($row[8]);
+    echo('<div class="rowone"><h4><span class="alignleft">'."\n");
+    }
 echo('<div class="rowone"><span class="alignright">');
 echo('Vol. ');
 echo($row[3]);
@@ -84,7 +130,7 @@ echo($row[6]);
 echo('<div class="rowtwo"><span class="alignleft">');
 //ISBN
 if($row[9] != 0 OR NULL){ 
-	echo('ISBN: '.$row[9]."\n");
+    echo('ISBN: '.$row[9]."\n");
 }
 echo('<div class="rowtwo"><span class="alignleft">');
 //Do we want this in? Probably need to format it nicely if we do
@@ -105,7 +151,7 @@ if (isset($_SESSION['collectorid']) ){
 
  if ($owned_result > 0) {
     $owned = "In My Library";
-	echo $owned;
+    echo $owned;
     }
  else {
     $owned = "not owned";
@@ -115,14 +161,14 @@ if (isset($_SESSION['collectorid']) ){
 //adding artists/authors to the record 
 
 $query5 = "SELECT author.firstname, author.lastname, author.authorID, role.rolename, role.roleID
-	FROM authorship JOIN author ON author.authorID = authorship.authorID
-	JOIN role ON role.roleID = authorship.roleID WHERE authorship.comicID = '$comicID' AND author.firstname IS NOT NULL";
+    FROM authorship JOIN author ON author.authorID = authorship.authorID
+    JOIN role ON role.roleID = authorship.roleID WHERE authorship.comicID = '$comicID' AND author.firstname IS NOT NULL";
 
 $result2 = mysql_query($query5);
 
 while($row2 = mysql_fetch_row($result2)){
-	echo ($row2[3].': '.$row2[0].'&nbsp;'.$row2[1]);
-	echo('<div class="rowtwo"><span class="alignleft">');
+    echo ($row2[3].': '.$row2[0].'&nbsp;'.$row2[1]);
+    echo('<div class="rowtwo"><span class="alignleft">');
 }
 
 echo('<div class="clear"></div>');
@@ -130,51 +176,7 @@ echo('</div>');
 echo('</span></div>');
 }
 ?>
-
-<!DOCTYPE html>
- <head>
- 	<html lang="en">
-    <meta charset="UTF-8">
-    <title>Cerebro - Your Brain on Comics</title>
-    <base href="http://localhost/SI664/cerebromockup/">
-    <link rel="stylesheet" type="text/css" href="static/css/styles.css" title="Default Stylesheet" media="all" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-    <script src="js/jquery.formalize.js"></script>
- </head>
- <body>
- 	<div id="wholecontainer">
-    	<header>
-    		<div id="header">
-    		<!-- search bar -->
-    		<div id="searchwrap">
-        		<input id="searchbox" type="search" />
-                <img src="static/images/searchicon.png" alt="Search Icon" height="19" width="19">
-        		<a href="advancedsearch.html">Advanced Search</a>
-        	</div>
-
-        	<div id="logo">
-        		<a href="index.php"><img src="static/images/cerebro_logo.gif" alt="Cerebro Logo"></a>
-        	</div>
-
-        	<!-- nav links -->
-        	<nav>
-            	<ul>
-                	<li><a href="index.php">Home</a></li>
-                	<li><a href="about.html">About</a></li>
-					<?php if (!isset($_SESSION['username'])){ ?>
-                	<li><a href="signin.php">Log In</a></li>
-					<li><a href="registration.php"></a></li>
-					<?php } ?>
-					<?php if ( isset($_SESSION['username'])){ ?>
-					<li><a href="logout.php">Logout</a></li>
-					<!-- Welcome user not quite working yet -->
-					<li>Welcome <?php '.htmlentities($_SESSION["username"]). ' ?></li>
-					<?php } ?>
-					   
-            	</ul>
-        	</nav>
-        	</div>
-    	</header>
+        </section>
 	    
 		<footer>
         	<p>&copy; 2013 Go Team Venture!</p>
